c1547c7e173478bf3a8148a49b6dd23550aaf55f
kaylasudza <121011338+kaylasudza@users.noreply.github.com>
2023-01-15 22:50:19 +0700

    Add files via upload

---
+++ src/main/java/Main/Connect.java
@@ -0,0 +1,50 @@
+package Main;
+
+import java.sql.*;
+
+public class Connect {
+	private final String USERNAME = "root";
+	private final String PASSWORD = "";
+	private final String DATABASE = "instantwork";
+	private final String HOST = "localhost:3306";
+	private final String CONNECTION = String.format("jdbc:mysql://%s/%s", HOST, DATABASE);
+	
+	private ResultSet rs;
+	
+	private Connection connection;
+	private Statement st;
+
+	
+	public Connect() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection(CONNECTION, USERNAME, PASSWORD);
+			st = connection.createStatement();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+                        System.out.println("TEST!");
+			e.printStackTrace();
+		}
+	}
+	
+	public ResultSet execQuery(String query) {
+		try {
+			rs = st.executeQuery(query);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return rs;
+	}
+	
+	public void executeUpdate(String query) {
+		try {
+			st.executeUpdate(query);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+}
